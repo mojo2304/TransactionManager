9e68e30c4c7045d1619454716606cd4a0ab3bba4
Johannes Hein <hein.johannes@web.de>
2014-01-18 21:21:01 +0100

    a new return Object for the GUIs

---
+++ src/tm/server/model/ResultGUI.java
@@ -0,0 +1,29 @@
+package tm.server.model;
+
+public class ResultGUI<T> {
+
+	// our Result
+	private T _result;
+	
+	// for Errors
+	private boolean _success;
+	private String _errorMessage;
+	
+	public ResultGUI(T r, boolean s, String e) {
+		_result = r;
+		_success = s;
+		_errorMessage = e;
+	}
+	
+	public boolean isSuccessful() {
+		return _success;
+	}
+	
+	public String getErrorMessage() {
+		return _errorMessage;
+	}
+	
+	public T getResult() {
+		return _result;
+	}
+}
